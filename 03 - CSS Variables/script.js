1ce54267a3496e5a0b87d3f1ac857def3ba50647
Sai-Kokate <saikokatelearning@gmail.com>
2023-06-05 10:51:14 +0530

    added javascript for 03 - CSS Variables

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,13 @@
+function handleChange() {
+  // console.log(this.dataset.sizing);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+const inputs = document.querySelectorAll(".controls input");
+
+inputs.forEach((input) => input.addEventListener("change", handleChange));
+inputs.forEach((input) => input.addEventListener("mousemove", handleChange));
